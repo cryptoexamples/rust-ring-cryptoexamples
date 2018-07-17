d04bc9020beecb8f073a49469a273c13328f4cf8
Philipp Keck <git@philippkeck.de>
2018-07-17 20:18:45 +0200

    Replace Result<>-based error handling with unwrap() calls to make the example(s) more copyable

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate base64;
 extern crate ring;
 
-mod errors;
 mod string_encryption_key_based_symmetric;
 
 fn main() {
     // This main method simply runs all the examples to ensure that they compile and work.
-    ::string_encryption_key_based_symmetric::main().unwrap();
+    ::string_encryption_key_based_symmetric::main();
 }
--- src/string_encryption_key_based_symmetric.rs
+++ src/string_encryption_key_based_symmetric.rs
@@ -1,12 +1,10 @@
-use errors::ExampleError;
-
 /// This example does the following:
 /// 1. Generate a random key for symmetric encryption. Normally you would only do this once and then either store the
 ///    key on the only machine that needs access to it (in order to implement an encrypted data storage on that machine)
 ///    or distribute the key to another machine so that encrypted messages can be sent and decrypted.
 /// 2. Use the key to encrypt some data. This corresponds to the saving or sending of data.
 /// 3. Use the key to decrypt the data again. This corresponds to loading or receiving the data.
-pub fn main() -> Result<(), ExampleError> {
+pub fn main() {
     use ring::aead;
     use ring::rand::{SecureRandom, SystemRandom};
     use std::io::prelude::*;
@@ -18,9 +16,9 @@ pub fn main() -> Result<(), ExampleError> {
     // GENERATE key and write it to the key file
     {
         let mut key: [u8; 32] = [0; 32];
-        SystemRandom::new().fill(&mut key)?;
-        let mut keyfile = File::create(keyfile)?;
-        keyfile.write_all(&key)?;
+        SystemRandom::new().fill(&mut key).unwrap();
+        let mut keyfile = File::create(keyfile).unwrap();
+        keyfile.write_all(&key).unwrap();
     }
 
     // The data to be encrypted
@@ -33,23 +31,23 @@ pub fn main() -> Result<(), ExampleError> {
     let nonce = {
         use std::iter::repeat;
         let mut nonce: Vec<u8> = repeat(0).take(algorithm.nonce_len()).collect();
-        SystemRandom::new().fill(&mut nonce)?;
+        SystemRandom::new().fill(&mut nonce).unwrap();
         nonce
     };
 
     // ENCRYPTION
     let ciphertext_base64 : String = {
         // LOAD key file for encryption
-        let mut file = File::open(keyfile)?;
+        let mut file = File::open(keyfile).unwrap();
         let mut key: [u8; 32] = [0; 32];
-        file.read_exact(&mut key)?;
+        file.read_exact(&mut key).unwrap();
         drop(file);
 
         // ENCRYPT the original data
-        let cipher = aead::SealingKey::new(algorithm, &key)?;
+        let cipher = aead::SealingKey::new(algorithm, &key).unwrap();
         let mut in_out = original_data.to_vec();
         in_out.resize(original_data.len() + algorithm.tag_len(), 0u8);
-        let ciphertext_length = aead::seal_in_place(&cipher, &nonce, &[0; 0], &mut in_out, algorithm.tag_len())?;
+        let ciphertext_length = aead::seal_in_place(&cipher, &nonce, &[0; 0], &mut in_out, algorithm.tag_len()).unwrap();
         let ciphertext = &in_out[..ciphertext_length];
 
         // CONVERT raw bytes to Base64 representation
@@ -66,13 +64,10 @@ pub fn main() -> Result<(), ExampleError> {
 
         // DECRYPT the ciphertext
         let cipher = aead::OpeningKey::new(algorithm, &key).unwrap();
-        let mut in_out = base64::decode(&ciphertext_base64)?;
-        let text_bytes = aead::open_in_place(&cipher, &nonce, &[0; 0], 0, &mut in_out)?;
-        String::from_utf8(text_bytes.to_vec())?
+        let mut in_out = base64::decode(&ciphertext_base64).unwrap();
+        let text_bytes = aead::open_in_place(&cipher, &nonce, &[0; 0], 0, &mut in_out).unwrap();
+        String::from_utf8(text_bytes.to_vec()).unwrap()
     };
 
-    if decrypted_data.as_bytes() != original_data {
-        return Err(ExampleError::AssertionMismatch());
-    }
-    return Ok(());
+    assert!(decrypted_data.as_bytes() == original_data);
 }
